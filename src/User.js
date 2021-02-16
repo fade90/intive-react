4de30f8c68a83942fce7143f50400c1e09b22d93
Federico Rodriguez <fede90.12@gmail.com>
2021-02-16 18:03:44 -0300

    se agrego boxshadow

--- src/User.js
+++ src/User.js
@@ -1,27 +1,20 @@
-import './User.css'
-import {Link} from 'react-router-dom'
+import "./User.css";
+import { Link } from "react-router-dom";
 
 const User = (props) => {
-  const fullName = `${props.name.title}. ${props.name.first} ${props.name.last}`
+  const fullName = `${props.name.title}. ${props.name.first} ${props.name.last}`;
   return (
     <div className="user-card">
-      <Link to={{ pathname: '/profile', state: props }}>
-      <img src={props.picture.large}/>
+      <Link to={{ pathname: "/profile", state: props }}>
+        <img src={props.picture.large} />
       </Link>
       <div className="heading-details">
-        <div className="name">
-          {fullName}
-        </div>
-        <div className="city">
-          {props.location.city}
-        </div>
-        <div className="country">
-          {props.location.country}
-        </div>
+        <div className="name">{fullName}</div>
+        <div className="city">{props.location.city}</div>
+        <div className="country">{props.location.country}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-
-export default User
+export default User;
--- src/UserDetail.js
+++ src/UserDetail.js
@@ -1,47 +1,44 @@
-import { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import { CSSTransition } from 'react-transition-group'
-import './UserDetail.css'
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { CSSTransition } from "react-transition-group";
+import "./UserDetail.css";
 
 const UserDetail = () => {
-    const [inProp, setInProp] = useState(false);
-    const { state: user } = useLocation()
-    const fullName = `${user.name.title}. ${user.name.first} ${user.name.last}`
-    const street = `${user.location.street.number} ${user.location.street.name}`
-        
-    useEffect(() => {
-        setInProp(true)
-    }, [])
+  const [inProp, setInProp] = useState(false);
+  const { state: user } = useLocation();
+  const fullName = `${user.name.title}. ${user.name.first} ${user.name.last}`;
+  const street = `${user.location.street.name}, ${user.location.street.number}`;
 
-    return (
-        <CSSTransition
-            classNames="user-detail"
-            in={inProp}
-            timeout={1000}
-            >
-            <div className="user-detail">
-                <div className="heading">
-                    <img src={user.picture.large}/>
-                    <div className="heading-details">
-                        <div className="name">
-                            {fullName}
-                        </div>
-                        <div className="email">
-                            <a href={`mailto:${user.email}`}>{user.email}</a>
-                        </div>
-                        <div className="phone">
-                            {user.phone}
-                        </div>
-                        <div className="street">
-                            {street}
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    setInProp(true);
+  }, []);
+
+  return (
+    <CSSTransition classNames="user-detail" in={inProp} timeout={1000}>
+      <div className="user-detail">
+        <div className="heading">
+          <img src={user.picture.large} />
+          <div className="heading-details">
+            <div className="name">{fullName}</div>
+            <div className="email">
+              <div className="email-title">Email</div>
+              <a href={`mailto:${user.email}`}>{user.email}</a>
             </div>
-        </CSSTransition>
-    )
-    
-}
+            <hr></hr>
+            <div className="phone">
+              <div className="phone-title">Phone</div>
+              {user.phone}
+            </div>
+            <hr></hr>
+            <div className="street">
+              <div className="street-title">Street</div>
+              {street}
+            </div>
+          </div>
+        </div>
+      </div>
+    </CSSTransition>
+  );
+};
 
-
-export default UserDetail
+export default UserDetail;
